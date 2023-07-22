d6865c2f5005d053f531c3b249cacbb1baef7cce
BLACKZEESH <119837813+BLACKZEESHx@users.noreply.github.com>
2023-07-22 22:07:06 +0500

    hmm

--- showtray.py
+++ showtray.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import *
+import ctypes
 from PyQt5.QtGui import *
 import win32gui
 from PyQt5.QtWidgets import QMainWindow, QApplication, QShortcut
@@ -23,11 +24,38 @@ class MainWindow(QMainWindow):
             self.setWindowFlag(Qt.WindowType.FramelessWindowHint, True)
             self.setWindowFlag(Qt.WindowType.Tool, True)
             self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
+            self.timer = QTimer(self)
+            self.timer.timeout.connect(self.timerbattime)
+            self.timer.start(100)
             self.ui.widget.setStyleSheet("*{\n"
 "    border-radius: 10px;\n"
 "    background-color: rgba(255, 255, 0, 0);\n"
 "     border: 4px solid rgb(255, 255, 255);\n"
 "}")
+        def timerbattime(self):
+            class SYSTEM_POWER_STATUS(ctypes.Structure):
+                _fields_ = [
+                    ("ACLineStatus", ctypes.c_byte),
+                    ("BatteryFlag", ctypes.c_byte),
+                    ("BatteryLifePercent", ctypes.c_byte),
+                    ("Reserved1", ctypes.c_byte),
+                    ("BatteryLifeTime", ctypes.c_ulong),
+                    ("BatteryFullLifeTime", ctypes.c_ulong),
+                ]
+
+            # create an instance of the SYSTEM_POWER_STATUS structure
+            status = SYSTEM_POWER_STATUS()
+
+            # call the GetSystemPowerStatus function to get the battery status
+            ctypes.windll.kernel32.GetSystemPowerStatus(ctypes.byref(status))
+
+            # extract the battery life percentage from the structure
+            battery_percent = status.BatteryLifePercent
+
+            # print the battery life percentage
+            # print("Battery Life:", battery_percent, "%")
+            self.ui.pushButton_3.setText(str(battery_percent)+"%")
+
 
 if __name__ == "__main__":
     app = QApplication([])
